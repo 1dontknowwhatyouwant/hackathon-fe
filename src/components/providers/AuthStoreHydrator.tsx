b1a966765a659105b5f722022b3dfb57775d7152
pro660 <gw904202@gmail.com>
2026-08-13 01:03:36 +0900

    앱 시작 시 인증 세션 복구 추가

--- src/components/providers/AuthStoreHydrator.tsx
+++ src/components/providers/AuthStoreHydrator.tsx
@@ -2,11 +2,24 @@
 
 import { useEffect } from "react";
 
+import { initializeAuthSession } from "@/lib/axios";
 import { useAuthStore } from "@/store/useAuthStore";
 
 export function AuthStoreHydrator() {
   useEffect(() => {
-    void useAuthStore.persist.rehydrate();
+    let active = true;
+
+    void (async () => {
+      await useAuthStore.persist.rehydrate();
+
+      if (active) {
+        await initializeAuthSession();
+      }
+    })();
+
+    return () => {
+      active = false;
+    };
   }, []);
 
   return null;
